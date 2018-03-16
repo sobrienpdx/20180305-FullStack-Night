d77f088e92e8e1719e3a2525c7109f154543e540
DED8IRD <ewu72829@gmail.com>
2018-03-15 20:32:05 -0700

    added solution to peaks and valleys

---
+++ 1 Python/solutions/lab18-peaks_and_valleys.py
@@ -0,0 +1,65 @@
+###
+# peaks_and_valleys.py
+# In class solution to lab 18: peaks and valleys
+# Takes in as input a list of heights
+# Returns the indices of peaks and valleys
+###
+
+def peaks(heights):
+    """
+    returns the indices of peaks
+    """
+    peaks = []
+    for i in range(1,len(heights)-1):
+        left = heights[i-1]
+        middle = heights[i]
+        right = heights[i+1]
+        if left < middle > right:
+            peaks.append(i)
+    return peaks
+
+
+def valleys(heights):
+    """
+    returns the indices of valleys
+    """
+    valleys = []
+    for i in range(1,len(heights)-1):
+        left = heights[i-1]
+        middle = heights[i]
+        right = heights[i+1]
+        if left > middle < right:
+            valleys.append(i)
+    return valleys
+
+
+def peaks_and_valleys(heights):
+    """
+    returns the indices of peaks and valleys in order appearance in the original data
+    """
+    p = peaks(heights)
+    v = valleys(heights)
+    return sorted(p + v)
+
+
+if __name__ == '__main__':
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+    print(data)
+    print(peaks(data))              # → [6, 14]
+    print(valleys(data))            # → [9, 17]
+    print(peaks_and_valleys(data))  # → [6, 9, 14, 17]
+    data = [1, 1, 1, 1, 1]
+    print(data)
+    print(peaks(data))              # → [] empty list
+    print(valleys(data))            # → []
+    print(peaks_and_valleys(data))  # → []
+    data = [3, 2, 1, 2, 3]
+    print(data)
+    print(peaks(data))              # → [] empty list
+    print(valleys(data))            # → [2]
+    print(peaks_and_valleys(data))  # → [2] 
+    data = [1, 2, 3, 2, 1]
+    print(data)
+    print(peaks(data))              # → [2] 
+    print(valleys(data))            # → []
+    print(peaks_and_valleys(data))  # → [2] 
