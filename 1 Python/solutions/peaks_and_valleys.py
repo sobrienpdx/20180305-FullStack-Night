42c8488e19b4267f2ebe2a88da4ff18b5de07fc6
DED8IRD <ewu72829@gmail.com>
2018-03-15 19:35:33 -0700

    added partial peaks_and_valleys lab

---
+++ 1 Python/solutions/peaks_and_valleys.py
@@ -0,0 +1,40 @@
+###
+# peaks_and_valleys.py
+# In class solution to lab 18: peaks and valleys
+# Takes in as input a list of heights
+# Returns the indices of peaks and valleys
+###
+
+def peaks(heights):
+	"""
+	returns the indices of peaks
+	"""
+	peaks = []
+	for i in range(1,len(heights)-1):
+		left = heights[i-1]
+		middle = heights[i]
+		right = heights[i+1]
+		if left < middle and right < middle:
+			peaks.append(i)
+	return peaks
+
+
+def valleys(heights):
+	"""
+	returns the indices of valleys
+	"""
+	pass
+
+
+def peaks_and_valleys(heights):
+	"""
+	returns the indices of peaks and valleys in order appearance in the original data
+	"""
+	pass
+
+
+if __name__ == '__main__':
+	data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+	print(peaks(data)) 				# → [6, 14]
+	print(valleys(data)) 			# → [9, 17]
+	print(peaks_and_valleys(data))	# → [6, 9, 14, 17]
