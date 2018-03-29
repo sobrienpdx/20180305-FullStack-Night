2355863d530792944f7dbfd8f7e6de55c27bcf9e
DED8IRD <ewu72829@gmail.com>
2018-03-29 15:09:18 -0700

    added lab 18 tictactoe

---
+++ 1 Python/labs/lab28-tictactoe.md
@@ -0,0 +1,65 @@
+# Lab 28: Tic-Tac-Toe 
+
+[Tic Tac Toe](https://en.wikipedia.org/wiki/Tic-tac-toe) is a game.
+Players take turns placing tokens (a 'O' or 'X') into a 3x3 grid.
+Whoever gets three in a row first wins.
+
+You will write a **Player** class and **Board** class to model Tic Tac Toe, and a function **main** that models gameplay taking in user inputs through REPL.
+
+
+The Player class has the following properties: 
+**name**:*player name*, **symbol**:*'X' or 'Y'*
+
+The Board class has the following properties:
+**board**:*your representation of the board*
+
+You can represent the board however you like, such as a 2D list, tuples, or dictionary.
+
+The Board class has the following methods:
+* `__repr__()` Returns a pretty string representation of the game board
+```py
+>>> print(board)
+X| | 
+O|X|O
+ | | 
+```
+
+* `place_token(x, y, token)` Place a token character string at a given coordinate (top-left is 0, 0), x is horizontal position, y is vertical position.
+
+```py
+>>> board.place_token(1, 2, 'X')
+ | | 
+ | |X
+ | | 
+```
+
+* `calc_winner()` What token character string has won or `None` if no one has.
+
+```py
+X| | 
+O|X|O
+ | |X
+>>> board.calc_winner()
+X
+```
+
+* `is_full()` Returns true if the game board is full.
+
+```
+X|O|X
+X|X|O
+O|O|X
+>>> board.is_full()
+True
+```
+
+* `is_game_over()` Returns true if the game board is full or a player has won.
+
+```
+X|O|X
+X|X|O
+O|O|X
+>>> board.is_game_over()
+True
+```
+
--- 1 Python/solutions/a_star.py
+++ 1 Python/solutions/a_star.py
@@ -1,11 +1,16 @@
 from collections import defaultdict, namedtuple
 
-# Chessboard position
-Position = namedtuple('Position', ['number', 'coord'])
+# # Chessboard position
+# Position = namedtuple('Position', ['number', 'coord'])
 
-# Chessboard
-Chessboard = [[Position(i+8*j, (i, j)) for i in range(8)] for j in range(8)]
+# # Chessboard
+# Chessboard = [[Position(i+8*j, (i, j)) for i in range(8)] for j in range(8)]
 
+Chessboard = {}
+for i in range(8):
+    for j in range(8):
+        Chessboard[i+8*j] = (i, j)
+        
 def Tree():
     """ Tree implementation using defaultdict
     """
@@ -15,7 +20,21 @@ def Tree():
 def h(start, goal):
     """ Returns manhattan distance heuristic
     """
-    return abs(goal[0]-start[0]) + abs(goal[1]-start[1])
+    (x1, y1) = Chessboard[start]
+    (x2, y2) = Chessboard[goal]
+    return abs(x1-x2) + abs(y1-y2)
+
+
+def get_neighbors(node):
+    """ Returns list of positions reachable with knight moves
+    """
+    knight_moves = [-17, -15, -10, -6, 6, 10, 15, 17] 
+    neighbors = []
+    for move in night_moves:
+        neighbor = node + move
+        if 0 <= neighbor < 64:
+            neighbors.append(neighbor)
+    return neighbors
 
 
 def a_star(start, goal):
@@ -34,13 +53,27 @@ def a_star(start, goal):
         # calculate f values for all nodes in the fringe
         options = [(node, g[current_node] + h(node, goal)) for node in fringe]
         options.sort(key=operator.itemgetter(1))
-        current_node = options[0][0]       # select node with lowest f value
+        # select node with lowest f value
+        current_node = options[0][0]
+
+        # exit condition
         if current_node == goal:
             return reconstruct_path(came_from, current_node)
+
+        # update fringe and visited sets
         fringe.remove(current_node)
         visited.add(current_node)
 
-        # neighbors = 
+        # explore neighbors of current node
+        neighbors = get_neighbors(current_node)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue  # ignore visited nodes
+            
+            if neighbor not in fringe:
+                fringe.add(neighbor)
+                g[neighbor] = came_from[]
+
 
 
 if __name__ == '__main__':
