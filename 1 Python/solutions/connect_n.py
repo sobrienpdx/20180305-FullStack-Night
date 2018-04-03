5e494a55b353c23c49674d437326451592c11d56
DED8IRD <ewu72829@gmail.com>
2018-04-02 21:49:39 -0700

    working connect n

--- 1 Python/solutions/connect_n.py
+++ 1 Python/solutions/connect_n.py
@@ -1,6 +1,7 @@
 # connect_n.py 
 # A combination of Tic Tac Toe and Connect Four: a two player game where to first player to get N in a row wins!
 # Constraints:  2 <= N <= 8, 3x3 <= board_size <= 8x8
+# Default: N=5, board=8x8
 
 import argparse, random
 
@@ -22,30 +23,18 @@ class Board:
         self.DEPTH = size
         self.WIDTH = size
         self.n = n
-        self.board = [[' ' for i in range(self.WIDTH)] for j in range(self.DEPTH)]
+        self.board = [[str(i+j*self.WIDTH+1).zfill(2) for i in range(self.WIDTH)] for j in range(self.DEPTH)]
 
     def __repr__(self):
         ret = ''
         for row in self.board:
-            ret += '|'.join(row)
+            for cell in row:
+                ret += str(cell) + '|'
             ret += '\n'
         return ret
-        
-        # ret = ''
-        # i = 1
-        # for row in self.board:
-        #     for cell in row:
-        #         if cell == ' ':
-        #             cell = i
-        #         ret += str(cell)
-        #         if i%3:
-        #             ret += '|'
-        #         i += 1
-        #     ret += '\n'
-        # return ret        
 
     def place_token(self, x, y, token):
-        if self.board[y][x] != ' ':
+        if type(self.board[y][x]) is Player:
             return 'Invalid move. Space taken.'
         else:
             self.board[y][x] = token
@@ -53,34 +42,31 @@ class Board:
     def calc_winner(self):
         # check horizontal wins
         for i in range(self.DEPTH):
-            for j in range(self.WIDTH-self.n-1):
-                chunk = self.board[i][j:j+n]
-                if all(item == self.board[i][j] and item != ' ' for item in chunk):
+            for j in range(self.WIDTH-self.n+1):
+                chunk = self.board[i][j:j+self.n]
+                if all(item == self.board[i][j] and not type(item) is Player for item in chunk):
                     return self.board[i][j].name
 
         # check vertical wins
         for j in range(self.WIDTH):  
-            for i in range(self.DEPTH-self.n-1):  
-                chunk = [(self.board[i+k][j] for k in range(self.n))]
-                if all(item == self.board[i][j] and item != ' ' for item in chunk):
+            for i in range(self.DEPTH-self.n+1):  
+                chunk = [self.board[i+k][j] for k in range(self.n)]
+                if all(item == self.board[i][j] and not type(item) is Player for item in chunk):
                     return self.board[i][j]
 
         # check diagonal wins
-        for i in range(self.DEPTH-self.n-1):
-            for j in range(self.WIDTH-self.n-1):
-                chunk = [chunk.append(self.board[i+k][j:j+self.n]) for k in range(self.n)]
-                # for row in chunk:
-                #     for cell in row:
-                #         print(cell, end='|')
-                #     print()
+        for i in range(self.DEPTH-self.n+1):
+            for j in range(self.WIDTH-self.n+1):
+                chunk = [self.board[i+k][j:j+self.n] for k in range(self.n)]
                 left_diag = True
                 right_diag = True
                 for k in range(self.n):
+                    print(type(chunk[k][k]))
                     if not left_diag and not right_diag :
-                        return False
-                    if chunk[k][k] != chunk[0][0] or chunk[k][k] == ' ':
+                        continue
+                    if chunk[k][k] != chunk[0][0] or not type(chunk[k][k]) is Player:
                         left_diag = False
-                    if chunk[self.n-k-1][k] != chunk[self.n-1][0] or chunk[self.n-k-1][k] == ' ':
+                    if chunk[self.n-k-1][k] != chunk[self.n-1][0] or not type(chunk[self.n-k-1][k]) is Player:
                         right_diag = False
                 if left_diag:
                     return chunk[0][0]
@@ -89,7 +75,7 @@ class Board:
 
     def is_full(self):
         for row in self.board:
-            if any(item==' ' for item in row):
+            if any(not type(item) is Player for item in row):
                 return False
         return True
 
@@ -99,19 +85,23 @@ class Board:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Connect N Game')
-    parser.add_argument('-n', help='Number to connect to win.')
-    parser.add_argument('-size', help='Size of game board (sizexsize)')
+    parser.add_argument('-n', type=int, choices=range(2, 9), help='Number to connect to win.')
+    parser.add_argument('-s', '--boardsize', type=int, choices=range(3, 9), help='Size of game board (size x size)')
     args = parser.parse_args()
 
-    grid_coord = {1:(0,0), 2:(1,0), 3:(2,0),
-                  4:(0,1), 5:(1,1), 6:(2,1),
-                  7:(0,2), 8:(1,2), 9:(2,2)}
-    move = 6
-    x, y = grid_coord[move]
     while True:
-        board = Board(3,3)
-        player_one = Player(input("Player one: "), 'X')
-        player_two = Player(input("Player two: "), 'O')
+        N = args.n if args.n else 5             # default N=5
+        SIZE = args.boardsize if args.boardsize else 8    # default 8x8 board
+        board = Board(N, SIZE)
+        grid_coord = {}
+        for y in range(board.DEPTH):
+            for x in range(board.WIDTH):
+                grid_coord[x+1+y*board.WIDTH] = (x, y)
+        # print(N, SIZE)
+        # print(grid_coord)
+
+        player_one = Player(input("Player one: "), '██')
+        player_two = Player(input("Player two: "), '░░')
         game_round = 1
 
         while not board.is_game_over():
@@ -121,9 +111,9 @@ if __name__ == '__main__':
                 move = input(f"{current_player.name}: Enter your move: ").strip()
                 try:
                     move = int(move)
-                    if 1 <= move <= 9:
+                    if 1 <= move <= SIZE**2:
                         x, y = grid_coord[move]
-                        move = board.place_token(x, y, current_player.token)
+                        move = board.place_token(x, y, current_player)
                         if type(move) is str:
                             raise IndexError('Invalid move.')
                         print(board)
@@ -135,7 +125,7 @@ if __name__ == '__main__':
                     # try:
                     #   x, y = move.split(',')
                     # except ValueError, IndexError:
-                        print("Invalid move. Please choose a sqaure [1-9] that isn't full.")
+                        print(f"Invalid move. Please choose a square [1-{SIZE**2}] that isn't full.")
 
         if not board.is_full():
             print(f"Game over! Winner: {board.calc_winner()}")
