239b446ea9728656231e77be2441b70aa659b4f0
DED8IRD <ewu72829@gmail.com>
2018-05-18 17:27:16 -0700

    added capstone docs

--- 4 Django/solutions/todosproj/todosite/todosapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodosappConfig(AppConfig):
-    name = 'todosapp'
--- 4 Django/solutions/todosproj/todosite/todosapp/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class TodoForm(forms.Form):
-    todo = forms.CharField(label='Todo', max_length=250)
